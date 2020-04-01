450ec501f736a034dd2636ff94b5882911f1ab7d
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 16:35:41 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -60,6 +60,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
+  binding.pry
   Movie.order("release_date > ? DESC", 2002)
 end
 
