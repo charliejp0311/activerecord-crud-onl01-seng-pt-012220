d1fcccf980f0d210676ac27c70e27f654d5f6cb8
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 15:35:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,9 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = args[title:]
-    m.release_date = args[release_date:]
+    args.each do |k,v|
+      m.k = v
+    end
   end
 end
 
