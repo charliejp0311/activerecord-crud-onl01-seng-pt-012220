889d8ae11c9aab1815807c3cf57fd8f84c8e1e8d
Charles Pelton <charliejp0311@gmail.com>
2020-04-01 16:50:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -83,7 +83,9 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movies.count.times do |i|
+    Movie.find_by_id(i).update(title: "A Movie")
+  end
 end
 
 def can_destroy_a_single_item
